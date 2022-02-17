7b08173820b866c0414ea2dc404a0799b5c4e80b
Sam Leeflang <sam.leeflang@naturalis.nl>
2022-02-17 13:50:11 +0100

    Fix URL geocase

--- src/main/java/eu/dissco/webflux/demo/service/webclients/GeoCaseService.java
+++ src/main/java/eu/dissco/webflux/demo/service/webclients/GeoCaseService.java
@@ -5,6 +5,10 @@ import eu.dissco.webflux.demo.Profiles;
 import eu.dissco.webflux.demo.domain.DarwinCore;
 import eu.dissco.webflux.demo.domain.Image;
 import eu.dissco.webflux.demo.properties.WebClientProperties;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Stream;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
@@ -12,59 +16,57 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.stream.Stream;
-
 @Slf4j
 @Service
 @AllArgsConstructor
 @Profile(Profiles.GEO_CASE)
-public class GeoCaseService implements WebClientInterface{
+public class GeoCaseService implements WebClientInterface {
 
-    private static final String RESPONSE = "response";
+  private static final String RESPONSE = "response";
 
-    private final WebClient webClient;
-    private final WebClientProperties properties;
+  private final WebClient webClient;
+  private final WebClientProperties properties;
 
-    public Stream<DarwinCore> retrieveData() {
-        var uri = properties.getEndpoint() + properties.getQueryParams();
-        return webClient.get().uri(uri + "&start=0").retrieve().bodyToFlux(JsonNode.class).expand(
-                response -> {
-                    var total = response.get(RESPONSE).get("numFound").asInt();
-                    var currentPosition = response.get(RESPONSE).get("start").asInt();
-                    if (currentPosition + properties.getItemsPerRequest() <= total) {
-                        currentPosition = currentPosition + properties.getItemsPerRequest();
-                        return webClient.get().uri(uri + "&start=" + currentPosition).retrieve().bodyToFlux(JsonNode.class);
-                    }
-                    return Flux.empty();
-                }).map(this::mapToDarwin).toStream().flatMap(Collection::stream);
+  public Stream<DarwinCore> retrieveData() {
+    var uri = properties.getEndpoint() + properties.getQueryParams();
+    return webClient.get().uri(uri + "&start=0&rows=" + properties.getItemsPerRequest()).retrieve()
+        .bodyToFlux(JsonNode.class).expand(
+            response -> {
+              var total = response.get(RESPONSE).get("numFound").asInt();
+              var currentPosition = response.get(RESPONSE).get("start").asInt();
+              if (currentPosition + properties.getItemsPerRequest() <= total) {
+                currentPosition = currentPosition + properties.getItemsPerRequest();
+                return webClient.get().uri(uri + "&start=" + currentPosition).retrieve()
+                    .bodyToFlux(JsonNode.class);
+              }
+              return Flux.empty();
+            }).map(this::mapToDarwin).toStream().flatMap(Collection::stream);
+  }
+
+  private List<DarwinCore> mapToDarwin(JsonNode response) {
+    var items = response.get(RESPONSE).get("docs");
+    var list = new ArrayList<DarwinCore>();
+    items.forEach(item -> {
+      var darwin = DarwinCore.builder()
+          .id(item.get("unitid").asText())
+          .scientificName(
+              item.has("fullscientificname") ? item.get("fullscientificname").asText() : null)
+          .basisOfRecord(item.has("recordbasis") ? item.get("recordbasis").asText() : null)
+          .institutionID(item.has("datasetowner") ? item.get("datasetowner").asText() : null)
+          .occurrenceID(item.has("recordURI") ? item.get("recordURI").asText() : null)
+          .images(item.has("images") ? getImages(item.get("images")) : null)
+          .build();
+      list.add(darwin);
+    });
+    return list;
+  }
+
+  private List<Image> getImages(JsonNode imagesNode) {
+    var images = new ArrayList<Image>();
+    for (JsonNode image : imagesNode) {
+      images.add(Image.builder().imageUri(image.asText()).build());
     }
-
-    private List<DarwinCore> mapToDarwin(JsonNode response) {
-        var items = response.get(RESPONSE).get("docs");
-        var list = new ArrayList<DarwinCore>();
-        items.forEach(item -> {
-            var darwin = DarwinCore.builder()
-                    .id(item.get("unitid").asText())
-                    .scientificName(item.has("fullscientificname") ? item.get("fullscientificname").asText() : null)
-                    .basisOfRecord(item.has("recordbasis") ? item.get("recordbasis").asText() : null)
-                    .institutionID(item.has("datasetowner") ? item.get("datasetowner").asText() : null)
-                    .occurrenceID(item.has("recordURI") ? item.get("recordURI").asText() : null)
-                    .images(item.has("images") ? getImages(item.get("images")) : null)
-                    .build();
-            list.add(darwin);
-        });
-        return list;
-    }
-
-    private List<Image> getImages(JsonNode imagesNode) {
-        var images = new ArrayList<Image>();
-        for (JsonNode image : imagesNode) {
-            images.add(Image.builder().imageUri(image.asText()).build());
-        }
-        return images;
-    }
+    return images;
+  }
 
 }
